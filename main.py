fix loss calculation!

main.py
@@ -14,12 +14,10 @@ from train import train
 from models.base_model import Net
 from torch.utils.data import DataLoader
 from utils import main_utils, train_utils
-from utils.main_utils import collate_fn
 from utils.train_logger import TrainLogger
 from omegaconf import DictConfig, OmegaConf
 
 torch.backends.cudnn.benchmark = True
-
 
 
 @hydra.main(config_path="config", config_name='config')
@@ -33,7 +31,7 @@ def main(cfg: DictConfig) -> None:
     logger.write(OmegaConf.to_yaml(cfg))
 
     # Set seed for results reproduction
-    main_utils.set_seed(cfg['main']['seed']) # TODO make sure
+    main_utils.set_seed(cfg['main']['seed'])  # TODO make sure
 
     # todo replace resnet - https://github.com/shilrley6/Faster-R-CNN-with-model-pretrained-on-Visual-Genome
 
@@ -48,7 +46,7 @@ def main(cfg: DictConfig) -> None:
     train_imgs = cfg['main']['train_paths']['processed_imgs']
     if not os.path.exists(train_imgs):
         logger.write("Processing train images")
-        preprocess_images(other_paths=cfg['main']['paths']['base_path'],
+        preprocess_images(other_paths=cfg['main']['paths'],
                           data_paths=cfg['main']['train_paths'],
                           image_size=cfg['train']['image_size'],
                           central_fraction=cfg['train']['central_fraction'],
@@ -57,7 +55,7 @@ def main(cfg: DictConfig) -> None:
     val_imgs = cfg['main']['val_paths']['processed_imgs']
     if not os.path.exists(val_imgs):
         logger.write("Processing validation images")
-        preprocess_images(other_paths=cfg['main']['paths']['base_path'],
+        preprocess_images(other_paths=cfg['main']['paths'],
                           data_paths=cfg['main']['val_paths'],
                           image_size=cfg['train']['image_size'],
                           central_fraction=cfg['train']['central_fraction'],
@@ -75,7 +73,6 @@ def main(cfg: DictConfig) -> None:
                               other_paths=cfg['main']['paths'],
                               logger=logger,
                               answerable_only=False)
-
 
     train_loader = DataLoader(dataset=train_dataset,
                               batch_size=cfg['train']['batch_size'],

train.py
@@ -160,11 +160,21 @@ def run_batch(model, log_softmax, batch_data, max_answers):
     # mean is over batch
     batch_size = y_hat.shape[0]
     batch_indices = []  # TODO cleaner way
-    for i in range(batch_size):
-        batch_indices.extend([i] * 10)
+    for i, a_len in enumerate(a_length):
+        batch_indices.extend([i] * a_len)  # batch indices are the 'x' indices equal to the number of actual different answers in each entry
         # a_indices = a.coalesce().indices().cpu().numpy()
     # a_values = a.coalesce().values()
-    batch_loss = (nll[[batch_indices, a_indices.flatten().cpu().numpy() - 1]] * (a_values.flatten() / 10.0)).sum() / batch_size
+    a_indices_flat = a_indices.flatten()
+    # remove indices that where part of padding, and adjust indices to match nll (start from 0 instead of 1)
+    a_indices_flat_nonzero_adjusted = a_indices_flat[a_indices_flat.nonzero()].flatten().cpu().numpy() - 1
+    nll_relevant = nll[[batch_indices, a_indices_flat_nonzero_adjusted]]
+    a_values_flat = a_values.flatten()
+
+    # remove values that where part of padding (0) and divide by 10 to get probabilities
+    a_values_flat_nonzero = a_values_flat[a_values_flat.nonzero()].flatten() / 10.0
+
+    #
+    batch_loss = (nll_relevant * a_values_flat_nonzero).sum() / batch_size
     # loss = (nll * a.to_dense() / 10).sum(dim=1).mean()
     # nll[a]
     batch_score = batch_accuracy(y_hat.data, (a_indices, a_values, (batch_size, max_answers)))  # TODO make sure calculation is correct according to Itai.